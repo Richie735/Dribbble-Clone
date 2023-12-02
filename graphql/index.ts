64a5c3ac2551ef1bf5c76afcee35e81e68fb59b0
Richie735 <93219607+Richie735@users.noreply.github.com>
2023-12-02 19:00:01 +0000

    Create fetchAllProjects and projectsQuery

--- graphql/index.ts
+++ graphql/index.ts
@@ -47,3 +47,33 @@ export const createProjectMutation = `
 		}
 	}
 `;
+
+export const projectsQuery = `
+  query getProjects($category: String, $endcursor: String) {
+    projectSearch(first: 8, after: $endcursor, filter: {category: {eq: $category}}) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        node {
+          title
+          sourceCodeUrl
+          description
+          liveSiteUrl
+          id
+          image
+          tag
+          createdBy {
+            id
+            email
+            name
+            avatarUrl
+          }
+        }
+      }
+    }
+  }
+`;
